9ac0138b5eb64acfae0b4c6b6976657e9c164110
Sheng Wu <shengwu34@gmail.com>
2018-10-21 01:17:21 -0700

    day 8

---
+++ advent/2016/day8.py
@@ -0,0 +1,223 @@
+# part 1
+# answer was 123
+
+
+inp = '''
+rect 1x1
+rotate row y=0 by 20
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 4
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 6
+rect 5x1
+rotate row y=0 by 2
+rect 1x3
+rotate row y=2 by 8
+rotate row y=0 by 8
+rotate column x=0 by 1
+rect 7x1
+rotate row y=2 by 24
+rotate row y=0 by 20
+rotate column x=5 by 1
+rotate column x=4 by 2
+rotate column x=2 by 2
+rotate column x=0 by 1
+rect 7x1
+rotate column x=34 by 2
+rotate column x=22 by 1
+rotate column x=15 by 1
+rotate row y=2 by 18
+rotate row y=0 by 12
+rotate column x=8 by 2
+rotate column x=7 by 1
+rotate column x=5 by 2
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate row y=3 by 28
+rotate row y=1 by 28
+rotate row y=0 by 20
+rotate column x=18 by 1
+rotate column x=15 by 1
+rotate column x=14 by 1
+rotate column x=13 by 1
+rotate column x=12 by 2
+rotate column x=10 by 3
+rotate column x=8 by 1
+rotate column x=7 by 2
+rotate column x=6 by 1
+rotate column x=5 by 1
+rotate column x=3 by 1
+rotate column x=2 by 2
+rotate column x=0 by 1
+rect 19x1
+rotate column x=34 by 2
+rotate column x=24 by 1
+rotate column x=23 by 1
+rotate column x=14 by 1
+rotate column x=9 by 2
+rotate column x=4 by 2
+rotate row y=3 by 5
+rotate row y=2 by 3
+rotate row y=1 by 7
+rotate row y=0 by 5
+rotate column x=0 by 2
+rect 3x2
+rotate column x=16 by 2
+rotate row y=3 by 27
+rotate row y=2 by 5
+rotate row y=0 by 20
+rotate column x=8 by 2
+rotate column x=7 by 1
+rotate column x=5 by 1
+rotate column x=3 by 3
+rotate column x=2 by 1
+rotate column x=1 by 2
+rotate column x=0 by 1
+rect 9x1
+rotate row y=4 by 42
+rotate row y=3 by 40
+rotate row y=1 by 30
+rotate row y=0 by 40
+rotate column x=37 by 2
+rotate column x=36 by 3
+rotate column x=35 by 1
+rotate column x=33 by 1
+rotate column x=32 by 1
+rotate column x=31 by 3
+rotate column x=30 by 1
+rotate column x=28 by 1
+rotate column x=27 by 1
+rotate column x=25 by 1
+rotate column x=23 by 3
+rotate column x=22 by 1
+rotate column x=21 by 1
+rotate column x=20 by 1
+rotate column x=18 by 1
+rotate column x=17 by 1
+rotate column x=16 by 3
+rotate column x=15 by 1
+rotate column x=13 by 1
+rotate column x=12 by 1
+rotate column x=11 by 2
+rotate column x=10 by 1
+rotate column x=8 by 1
+rotate column x=7 by 2
+rotate column x=5 by 1
+rotate column x=3 by 3
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 39x1
+rotate column x=44 by 2
+rotate column x=42 by 2
+rotate column x=35 by 5
+rotate column x=34 by 2
+rotate column x=32 by 2
+rotate column x=29 by 2
+rotate column x=25 by 5
+rotate column x=24 by 2
+rotate column x=19 by 2
+rotate column x=15 by 4
+rotate column x=14 by 2
+rotate column x=12 by 3
+rotate column x=9 by 2
+rotate column x=5 by 5
+rotate column x=4 by 2
+rotate row y=5 by 5
+rotate row y=4 by 38
+rotate row y=3 by 10
+rotate row y=2 by 46
+rotate row y=1 by 10
+rotate column x=48 by 4
+rotate column x=47 by 3
+rotate column x=46 by 3
+rotate column x=45 by 1
+rotate column x=43 by 1
+rotate column x=37 by 5
+rotate column x=36 by 5
+rotate column x=35 by 4
+rotate column x=33 by 1
+rotate column x=32 by 5
+rotate column x=31 by 5
+rotate column x=28 by 5
+rotate column x=27 by 5
+rotate column x=26 by 3
+rotate column x=25 by 4
+rotate column x=23 by 1
+rotate column x=17 by 5
+rotate column x=16 by 5
+rotate column x=13 by 1
+rotate column x=12 by 5
+rotate column x=11 by 5
+rotate column x=3 by 1
+rotate column x=0 by 1
+'''
+
+commands = inp.strip().split('\n')
+
+screen = []
+for _ in range(6):
+    screen.append([False] * 50)
+
+def print_screen():
+    for i in range(len(screen)):
+        for j in range(len(screen[0])):
+            if screen[i][j]:
+                #print('#',)
+                print '#',
+            else:
+                #print('.',)
+                print '.',
+        print
+
+for command in commands:
+    parts = command.split()
+    if parts[0] == 'rect':
+        x, y = (int(dim) for dim in parts[1].split('x'))
+        for i in range(y):
+            for j in range(x):
+                screen[i][j] = True
+    elif parts[0] == 'rotate':
+        if parts[1] == 'row':
+            y = int(parts[2][2:])
+            shift = int(parts[4])
+            screen[y] = screen[y][-shift:] + screen[y][:-shift]
+        elif parts[1] == 'column':
+            x = int(parts[2][2:])
+            shift = int(parts[4])
+            elems = [screen[0][x], screen[1][x], screen[2][x], screen[3][x], screen[4][x], screen[5][x]]
+            shifted = elems[-shift:] + elems[:-shift]
+            for i in range(6):
+                screen[i][x] = shifted[i]
+
+print(sum(sum(bool(elem) for elem in row) for row in screen))
+
+# part 2
+# answer was AFBUPZBJPS
+
+print_screen()
